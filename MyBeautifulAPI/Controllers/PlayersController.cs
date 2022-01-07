1abbc3899f4f3ed88f7be5e453beaaeda820d59b
Antonnn95 <34644124+Antonnn95@users.noreply.github.com>
2022-01-07 11:35:32 +0100

    Add Repo

---
+++ MyBeautifulAPI/Controllers/PlayersController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyBeautifulAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBeautifulAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlayersController : ControllerBase
+    {
+        [HttpGet]
+        public List<Player> Get(string country, string name)
+        {
+            List<Player> players = new List<Player>()
+            {
+                new Player() { Name = "Robert", Id = 1, Country = "Sweden", Score = 24 },
+                new Player() { Name = "Winston", Id = 2, Country = "Germany", Score = -1 },
+                new Player() { Name = "Leonard", Id = 3, Country = "Russia", Score = 100 }
+            };
+
+            if (!string.IsNullOrWhiteSpace(country))
+            {
+                players = players.Where(p => p.Country == country).ToList();
+            }
+
+            if (!string.IsNullOrWhiteSpace(name))
+            {
+                players = players.Where(p => p.Name == name).ToList();
+            }
+
+            return players;
+        }
+    }
+}
---
+++ MyBeautifulAPI/Models/Team.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBeautifulAPI.Models
+{
+    public class Team
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Player> players { get; set; }
+    }
+}
